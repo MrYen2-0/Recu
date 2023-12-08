5f3231631e29eeeb00eccbbfb25ca7a4d156515e
MrYen2-0 <223225@ids.upchiapas.edu.mx>
2023-12-08 15:22:52 -0600

    Listo

--- app/pages/cliente/abogados/page.jsx
+++ app/pages/cliente/abogados/page.jsx
@@ -1,64 +1,43 @@
 "use client";
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../../styles/clientes/abogado.css';
 import Header from '../../../components/componentes-cliente/Header';
-import { useEffect } from 'react';
-import axios from 'axios';
+import Abogados from "../../components/abogados/Abogados";
 
 function Page() {
-
     const [abogados,setAbogados] = useState([]);
 
     const displayAbogados = async () => {
       try {
         const response = await fetch("https://api-aboweb-yenter.onrender.com/abogado/getAll");
+        
         if (!response.ok) {
-          console.log("no hay abogados");
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+  
+        const data = await response.json();
+  
+        if (data.resultado.length === 0) {
+          console.log("No hay abogados");
           return;
         }
-    
-        const responseData = await response.json();
-    
-        if (responseData.resultado.length === 0) {
-          console.log("no hay abogados");
-          return;
-        }
-    
-        let abogados = [];
-        let i = 0;
-    
-        responseData.resultado.forEach((abogado) => {
-          abogados[i] = (
-            <div className="cuadros-abogados" key={i}>
-              <div className="texto-abogado">
-                <div className="titulo-abogado">
-                  {abogado.nombre}
-                </div>
-                <div className="descripcion-abogado">
-                  {abogado.descripcion}
-                </div>
-              </div>
-              <div className="imagen-abogado">
-                <img src="/images/persona.png" className="persona-abogado" alt="persona-abogado" />
-              </div>
-              <div className="areas-abogado">
-                {abogado.area}
-              </div>
-            </div>);
-          i++;
-        });
-    
+  
+        let abogados = data.resultado.map((abogado) => ({
+          _id: abogado._id,
+          nombre: abogado.nombre,
+          descripcion: abogado.descripcion,
+          area: abogado.area,
+        }));
+  
         setAbogados(abogados);
       } catch (error) {
-        // Reemplazar la función alert por el componente Alert
         alert(error.message);
       }
     };
-    
-
-      useEffect(() => {
-        displayAbogados();
-      },[]);
+  
+    useEffect(() => {
+      displayAbogados();
+    }, []);
 
   return (
     <div>
@@ -66,18 +45,13 @@ function Page() {
       <div>
       <Header />
     </div>
-<div className="contenedor-tituloabogados">
-  <div className="titulos">
-    <div className="titulo3">Abogados </div>
-    <div className="titulo4">De la firma</div>
-  </div>
-</div>
-<div className="contenedor-abogados">
-    <div className="imagenes-abogados">
-        {abogados}
-    </div>
-</div>
-
+    <div className="contenedor-tituloabogados">
+          <div className="titulos">
+            <div className="titulo3">Abogados </div>
+            <div className="titulo4">De la firma</div>
+          </div>
+        </div>
+        <Abogados abogadosData={abogados} />
     </div>
   </div>
   );
